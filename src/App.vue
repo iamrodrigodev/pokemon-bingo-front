b1107a32464ab5c1d00e4cb3011902ccb61b1c9b
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-05-21 01:42:51 -0500

    refactor(app): migrar diseño principal a tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <h1
-        style="display: flex; align-items: end; justify-content: center; font-size: 3rem; margin: 0"
-      >
-        <img src="/pokemon-logo.png" alt="" width="200" style="margin-right: 10px" />
-        Bingo
+  <div id="app" class="text-center text-slate-800 antialiased font-sans flex flex-col min-h-screen">
+    <div id="nav" class="p-8">
+      <h1 class="flex items-end justify-center text-5xl m-0">
+        <img src="/pokemon-logo.png" alt="Pokemon Logo" class="w-[200px] mr-3" />
+        <span class="font-bold">Bingo</span>
       </h1>
     </div>
-    <div class="page-wrapper">
+    <div class="page-wrapper flex-grow p-4 flex flex-col items-center">
       <router-view />
     </div>
   </div>
@@ -24,45 +22,4 @@ onMounted(() => {
 });
 </script>
 
-<style>
-*,
-*::after,
-*::before {
-  box-sizing: border-box;
-}
-html,
-body {
-  min-height: 100%;
-}
-body {
-  background: url('/bg.jpg') no-repeat !important;
-  background-size: cover !important;
-  background-position: center bottom !important;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-.box {
-  background: white;
-  border-radius: 10px;
-  padding: 10px;
-  box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.1);
-}
-</style>
