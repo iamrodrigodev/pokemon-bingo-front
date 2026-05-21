c2e47ed0804c524669917a6cab945c091310b327
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-05-21 01:42:51 -0500

    refactor(supabase): extraer url a variables de entorno

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 declare global {
   interface ImportMetaEnv {
+    readonly VITE_SUPABASE_URL: string;
     readonly VITE_SUPABASE_KEY: string;
   }
   interface ImportMeta {
@@ -9,7 +10,7 @@ declare global {
 
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = 'https://dtnxfgjbwjgtyqievaob.supabase.co';
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
