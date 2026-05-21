6c51973e4b77077d333e717c59a67fa25f2414bb
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-05-21 01:42:52 -0500

    refactor(main): importar estilos globales de scss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { store, storeKey } from './store';
+import './assets/styles/main.scss';
 
 const app = createApp(App);
 
